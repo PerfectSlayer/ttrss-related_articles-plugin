ac949ca2a62a6876efb1df85a589275aba0b25b8
PerfectSlayer <bruce.bujon@gmail.com>
2019-03-29 20:58:57 +0100

    Update sql installation file

--- schema.sql
+++ schema.sql
@@ -1,14 +1,1 @@
-SET NAMES utf8;
-SET CHARACTER SET utf8;
-
-drop table if exists plugin_ttrss_related_articles;
-
-CREATE TABLE `plugin_ttrss_related_articles` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `ref_id` int(11) NOT NULL,
- `title` text NOT NULL,
- `content` longtext NOT NULL,
- PRIMARY KEY (`id`),
- KEY `ref_id` (`ref_id`),
- FULLTEXT KEY `title` (`title`,`content`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
+ALTER TABLE `hardcodiblog`.`ttrss_entries` ADD FULLTEXT `ttrss_related_articles` (`title`, `content`);
